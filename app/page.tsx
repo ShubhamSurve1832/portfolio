4e6dbfe0d68137ab20380f974593285f6c5537a1
Devloper-Sigmoid <145331707+Devloper-Sigmoid@users.noreply.github.com>
2024-09-24 10:38:50 +0530

    Add initial setup files

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
+import Grid from "@/components/Grid";
 
+const navItems = [{
+  name: "Home",
+  link: "/",
+  icon: '🏠',
+},
+{
+  name: "About",
+  link: "/about",
+  icon: '👤',
+},
+{
+  name: "Contact",
+  link: "/contact",
+  icon: '📧',
+},
+]
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
-        <h1 className="text-9xl">Hello World</h1>
+        <FloatingNavbar navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MagicButton = ({
+    title, icon, position, handleClick, className
+}: { title: string, icon: React.ReactNode, position: string, handleClick: () => void, className?: string }) => {
+    return (
+        <>
+            <button className={`relative w-full flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none gap-2 ${className}`}>
+                <div> {position === "left" && icon}</div>
+                <p>{title}</p>
+                <div> {position === "right" && icon}</div>
+
+            </button>
+        </>
+    )
+}
+
+export default MagicButton
