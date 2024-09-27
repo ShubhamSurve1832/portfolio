8d274c7267a7eef386a08e3e5c880cd82ca9e87d
Devloper-Sigmoid <145331707+Devloper-Sigmoid@users.noreply.github.com>
2024-09-27 16:20:19 +0530

    vehicle listing filter added

--- data/index.ts
+++ data/index.ts
@@ -9,7 +9,7 @@ export const navItems = [
     {
       id: 1,
       title: "I prioritize client collaboration, fostering open communication ",
-      description: "",
+      description: "test",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
