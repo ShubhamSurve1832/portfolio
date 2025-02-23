254ed9ed53fd74aa245baeff74f111f026f3b134
devil <shubhamsurve1832@gmail.com>
2025-02-23 22:37:33 +0530

    testimonial section changes

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -8,10 +8,10 @@ const Clients = () => {
       <h2 className='heading'>
         Kinjd Words from <span className='text-purple'> Satisfied Clients</span>
       </h2>
-      <div className='flex flex-col items-center'>
-        <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center overflow-hidden'>
+      <div className='flex flex-col items-center max-lg:mt-10'>
+       
         <InfiniteMovingCards items={testimonials} className='w-full' speed='slow' />
-        </div>
+        
       </div>
     </div>
   )
